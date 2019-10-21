f096ba85c481f974e600dc2d8d90561ba177cf74
Stefan-Dorin Camarasu <stefan.camarasu@tremend.com>
2019-10-21 23:30:36 +0300

    try subjects

--- src/code.ts
+++ src/code.ts
@@ -1,21 +1,60 @@
-import { observable, Observable } from 'rxjs'; 
+import { observable, Observable, fromEvent } from 'rxjs'; 
+import { Subject } from 'rxjs';
+// import { setInterval } from 'timers';
 // import * as Rx from 'rxjs';
+
+function addItem(val:any) {
+  var node = document.createElement("li");
+  var textnode = document.createTextNode(val);
+  node.appendChild(textnode);
+  document.getElementById("output").appendChild(node);
+}
 
 const obs$ = Observable.create((observer:any) => {
   try {
     observer.next('testin testn\'');
     observer.next('flexn flexn');
-    throw 'stuff';
-    observer.complete();
-    
-    observer.next('This will not send');
+    // observer.complete();
+    setInterval(()=> {
+      observer.next('...running ')
+    }, 2000);    
   } catch(err) {
     observer.error(err)
   }
 });
 
+const mouseM$ = fromEvent(document, 'click');
+
 
 const sub1 = obs$.subscribe(
-  (resolved:any) => console.log(resolved)
+  (resolved:any) => {
+    // console.log(resolved)
+    // addItem(resolved);
+  }
 )
+
+// setTimeout(()=> {
+//   const sub2 = obs$.subscribe(
+//     (resolved:any) => {
+//       addItem(resolved + ' from sub2 !!');
+//     }
+//   )
+// }, 4000)
+
+setTimeout(() => {
+  sub1.unsubscribe();
+}, 8001);
+
+/////// with subject 
+const subject1 = new Subject();
+
+subject1.subscribe(
+  success => console.log(success),
+);
+
+subject1.next('send 1')
+subject1.next('send 2')
+
+
+
 
