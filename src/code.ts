d5e59392791f55ad18ea482f17614316296c3ac3
Stefan-Dorin Camarasu <t026camas@RBRO.RBG.CC>
2019-10-28 18:56:41 +0200

    redo algorithm for names sugestions

--- src/code.ts
+++ src/code.ts
@@ -57,107 +57,266 @@ subject1.next('send 2')
 
 
 
+"use strict";
+
+let reg = /[^a-zA-Z\s-]/gi;
+
 class NameEditing {
-  firstNames:string[]
-  lastNames:string[];
-  result:string[];
-
-  static regex = /[^a-zA-Z\s-]/gi;
-
-
   constructor() {
-
     this.firstNames = [];
     this.lastNames = [];
     this.result = [];
   }
 
-  static joinBySpace(first:string, last:string) {
+  static joinBySpace(first, last) {
     return `${first} ${last}`;
   }
 
-
-  static acceptByLength(first:string, last:string, len=25) {
-    let value=NameEditing.joinBySpace(first, last);
-    if (value.length <= len) {
-      return value;
-    } else {
-      const initial = first[0].toLocaleUpperCase();
-      return NameEditing.joinBySpace(initial, last).substr(0, len);
-    }
+  static acceptByLength(name, maxLen=25) {
+    return (name && name.length <= maxLen) ? name : '';
   }
 
-  createNameList(firstName:string, lastName:string) {
-    let fullFName = firstName.replace(NameEditing.regex, '');
-    // this.firstNames.push(fullFName.trim());
-  
-    let fullLName = lastName.replace(NameEditing.regex, '');
-    // this.lastNames.push(fullLName.trim());
-    
-    //@cheeck for dash-combo
-    // first combo
-    let splitsBySpace_fn = fullFName.split(' ').filter(word => word.length);
-    for (let s of splitsBySpace_fn) {
-      this.firstNames.push(...s.split('-'))
+  static mergeEvenOdds(collectionEven=[], collectionOdds=[], index=0) {
+    let evenIterator = index;
+    let oddsIterator = index + 1;
+    const result = [];
+    collectionEven.forEach((val) => {
+      result[evenIterator] = val;
+      evenIterator += 2;
+    });
+    collectionOdds.forEach((val) => {
+      result[oddsIterator] = val;
+      oddsIterator += 2;
+    });
+    return result;
+  }
+
+  static joinByDash(first, last) {
+    return [`${first}-${last}`, `${last}-${first}`];
+  }
+
+  createSeparateFirstNames(name) {
+    const splitsBySpace = name.split(' ').filter(word => word.length);
+
+    // for (const word of splitsBySpace) {
+    //   // add full name
+    //   if (word.length) this.firstNames.push(word);
+    // }
+
+    const allNames = [];
+    for (const word of /* this.firstNames */ splitsBySpace) {
+      const dashed = word.split('-');
+      if (dashed[0].length && dashed[0].length) allNames.push(dashed[0]);
+      if (dashed[1] && dashed[1].length) allNames.push(dashed[1]);
     }
+
+    // @TODO use functional statics
+    this.firstNames.push(...allNames);
     this.firstNames = this.firstNames
       .filter(word => word.length)
-    // unique items
-      .filter((word, idx, all) => all.indexOf(word) === idx)
-  
-    //@refactor to one liners ?
-    let splitsBySpace_ln = fullLName.split(' ').filter(word => word.length);
-    for (let s of splitsBySpace_ln) {
-      this.lastNames.push(...s.split('-').filter(word => word.length))
-    }
-
-    for (let l of this.lastNames) {
-      for (let f of this.firstNames) {
-        this.result.push(NameEditing.acceptByLength(f,l));
-      }
-    }
-    
-    // after all chartezian prod
-    console.log(this.result)
+      // unique items
+      .filter((word, idx, all) => all.indexOf(word) === idx);
+    console.log("firstNames", this.firstNames);  
   }
 
+  createSeparateLastNames(name) {
+    /* ... */
+    const splitsBySpace = name.split(' ').filter(word => word.length);
+
+    // for (const word of splitsBySpace) {
+    //   // add full name
+    //   if (word.length) this.firstNames.push(word);
+    // }
+
+    const allNames = [];
+    for (const word of /* this.lastNames */ splitsBySpace) {
+      const dashed = word.split('-');
+      if (dashed[0].length) allNames.push(dashed[0]);
+      if (dashed[1] && dashed[1].length) allNames.push(dashed[1]);
+    }
+
+    // @TODO use functional statics
+    this.lastNames.push(...allNames);
+    this.lastNames = this.lastNames
+      .filter(word => word.length)
+      // unique items
+      .filter((word, idx, all) => all.indexOf(word) === idx);
+    console.log("lastNames", this.lastNames);  
+  }
+
+  createNameList(firstName, lastName) {
+    /* run init() */
+    const fn = firstName.replace(reg, '');
+    const ln = lastName.replace(reg, '');
+
+    this.createSeparateFirstNames(fn);
+    this.createSeparateLastNames(ln);
+
+    // @select and push by iterators odds and evens combined
+
+    if (this.lastNames.length && this.firstNames.length) {
+      const _lastNames = [...this.lastNames];
+      const _firstNames = [...this.firstNames];
+
+      let forward = [];
+      for (const l of _lastNames) {
+        for (const f of _firstNames) {
+          forward.push(NameEditing.joinBySpace(f, l));
+          forward.push(NameEditing.joinBySpace(l, f));
+        }
+      }
+
+      let reversed = [];
+      for (const f of _firstNames) {
+        for (const l of _lastNames) {
+          reversed.push(NameEditing.joinBySpace(f, l));
+          reversed.push(NameEditing.joinBySpace(l, f));
+        }
+      }
+
+      forward = forward
+        .filter(word => word.length)
+        .filter((word, idx, all) => all.indexOf(word) === idx);
+
+      reversed = reversed
+        .filter(word => word.length)
+        .filter((word, idx, all) => all.indexOf(word) === idx);
+
+      // @push separate names
+      this.result.push(...NameEditing.mergeEvenOdds(forward, reversed)
+        .filter(word => word.length && word.length <= 25));
+
+      // @push firstNames by lastName
+      // Priority 2
+      const priorityFn = [];
+      this.firstNames.forEach((_fn) => {
+        priorityFn.push(NameEditing.acceptByLength(NameEditing.joinBySpace(_fn, ln)));
+        priorityFn.push(NameEditing.acceptByLength(NameEditing.joinBySpace(ln, _fn)));
+      });
+      this.result.unshift(...priorityFn)
+      this.result = this.result
+        .filter(word => word.length && word.length <= 25)
+        .filter((word, idx, all) => all.indexOf(word) === idx);
+      console.log("after merged odds", this.result)  
+    }
+    /** ========================================================== */
+
+    // @select and push priority names
+    // @push first clean names as first priority
+    // Priority 1
+
+    if (this.result.length) {
+      this.result = this.result.map(word => word.toLocaleUpperCase());
+      // @exit here
+    } else {
+      // fallback for very long names
+      // for very long Last names and short first name
+      for (const _ln of this.lastNames) {
+        for (const _fn of this.firstNames) {
+          this.result.push(...NameEditing.acceptByHighPriority(_fn, _ln))
+         
+          this.result = this.result
+            .filter(word => word.length && word.length <= 25)
+            .filter((word, idx, all) => all.indexOf(word) === idx);
+        }
+      }
+
+      if (this.result.length) {
+        this.result = this.result.map(word => word.toLocaleUpperCase());
+        // @exit here
+      } else {
+        // fallback to initials
+        for (const _ln of this.lastNames) {
+          for (const _fn of this.firstNames) {
+            const initial = _fn[0];
+            this.result.push(NameEditing.joinBySpace(initial, _ln), NameEditing.joinBySpace(_ln, initial))
+            this.result = this.result
+              .filter(word => word.length && word.length <= 25)
+              .filter((word, idx, all) => all.indexOf(word) === idx);
+          }
+        }
+        
+        if (this.result.length) {
+          this.result = this.result.map(word => word.toLocaleUpperCase());
+          // @exit here
+        } else {
+          // fallback for very long last names only
+          for (const _ln of this.lastNames) {
+            this.result.push(_ln.substr(0, 25));
+          }
+          
+        }
+      }
+
+      // clear all again
+      this.result = this.result
+        .filter(word => word.length)
+        .filter((word, idx, all) => all.indexOf(word) === idx)
+        .map(word => word.toLocaleUpperCase());
+        console.log("truncated last", this.result);
+    }
+  }
+
+  static acceptByHighPriority(fn, ln) {
+    const name = NameEditing.acceptByLength(NameEditing.joinBySpace(fn, ln));
+    if (name.length) {
+      const nameRev = NameEditing.joinBySpace(fn, ln);
+      return [name, nameRev];
+    }
+    return '';
+  }
+
+  get list() {
+    return this.result;
+    
+  }
 }
+
+const nList = new NameEditing();
+nList.createNameList("Viorel Marius Ianis", "Constantin Elias David");
+console.log(nList.list.slice(0,10));
 
 const n = new NameEditing();
-n.createNameList('Ion-Ion john  Ultrameganamamescovici', 'Gigea Tiriac&__1Brontozaurix');
+n.createNameList('Jan-Pierre', 'Peter-Jack Jacksovicovichi');
 // =================== <> =================== //
 
-function formatNameList(firstName:string, lastName:string) {
-  const regex = /[^a-zA-Z\s-]/gi;
-  let firstNames = [];
-  let lastNames = [];
 
-  let fullFName = firstName.replace(regex, '');
-  firstNames.push(fullFName.trim());
+/**
+ * First Name: "John-Dave"
+Last Name: "VeryVeryLongLongNamer-ExtraExtraLongLongNamey"
+▿ 8 elements
+ - "J VERYVERYLONGLONGNAMER"
+ - "VERYVERYLONGLONGNAMER J"
+ - "D VERYVERYLONGLONGNAMER"
+ - "VERYVERYLONGLONGNAMER D"
+ - "J EXTRAEXTRALONGLONGNAMEY"
+ - "EXTRAEXTRALONGLONGNAMEY J"
+ - "D EXTRAEXTRALONGLONGNAMEY"
+ - "EXTRAEXTRALONGLONGNAMEY D"
 
-  let fullLName = lastName.replace(regex, '');
-  lastNames.push(fullLName.trim());
+Generating suggestions for:
 
+First Name: "John-Dave"
+Last Name: "VeryVeryLongLongLastNameName-AnotherVeryVeryLongNameName"
+▿ 2 elements
+ - "VERYVERYLONGLONGLASTNAMEN"
+ - "ANOTHERVERYVERYLONGNAMENA"
 
-  // result.push(joinBySpace(fullFName, fullLName));
-  // result.push(joinBySpace(fullLName, fullFName));
+Generating suggestions for:
 
-  let splitsBySpace = fullFName.split(' ').filter(word => word.length);
-  firstNames.push(...splitsBySpace);
-
-
-  let splitsByDash:any = [];
-
-  for (const n of splitsBySpace) {
-    const _s = n.split('-');
-    // console.log('splits by dash: ', _s, _s.length);
-    if (_s.length > 1) splitsByDash.push(..._s);
-    // console.log('splits by dash ->>after ', splitsByDash);
-  };
-
-  firstNames.push(...splitsByDash);
-
-}
-
-formatNameList('Ion-Ion  Ultrameganamamescovici', 'Tiriac&__1Brontozaurix');
-
+First Name: "Viorel Marius Ianis"
+Last Name: "Constantin Elias David"
+▿ 10 elements
+ - "VIOREL CONSTANTIN"
+ - "CONSTANTIN VIOREL"
+ - "MARIUS CONSTANTIN"
+ - "CONSTANTIN MARIUS"
+ - "IANIS CONSTANTIN"
+ - "CONSTANTIN IANIS"
+ - "VIOREL ELIAS"
+ - "ELIAS VIOREL"
+ - "VIOREL DAVID"
+ - "DAVID VIOREL"
+ * 
+ * 
+ */
